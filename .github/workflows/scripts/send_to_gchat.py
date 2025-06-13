5f2fdf48062af34e3bd90b2a8eab81ddade6b96d
Atirson Fabiano <fabiano.oliveira2213@gmail.com>
2025-06-13 09:20:31 -0300

    Create send_to_gchat.py

---
+++ .github/workflows/scripts/send_to_gchat.py
@@ -0,0 +1,14 @@
+import os
+import requests
+#teste
+GCHAT_WEBHOOK_URL = os.environ['GCHAT_WEBHOOK_URL']
+PR_TITLE = os.environ['PR_TITLE']
+PR_URL = os.environ['PR_URL']
+PR_AUTHOR = os.environ['PR_AUTHOR']
+
+message = {
+    "text": f"@all\nNovo Pull Request aberto por *{PR_AUTHOR}*:\n*{PR_TITLE}*\nVeja mais: {PR_URL}"
+}
+
+response = requests.post(GCHAT_WEBHOOK_URL, json=message)
+response.raise_for_status()
